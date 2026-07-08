59e27ffe9323382dc260afba0356548852e3900a
aleena-jaison <abi.alif@acetvm.com>
2026-07-08 21:58:44 +0530

    countries udpated

--- lib/countries.ts
+++ lib/countries.ts
@@ -3,5 +3,6 @@ export const WORLD_CUP_TEAMS = [
   "Costa Rica", "Croatia", "Denmark", "Ecuador", "England", "France",
   "Germany", "Ghana", "Iran", "Japan", "Mexico", "Morocco", "Netherlands", "Cape Verda",
   "Poland", "Portugal", "Qatar", "Saudi Arabia", "Senegal", "Serbia", "Italy", "Germany", "Netherlands", 
-  "South Korea", "Spain", "Switzerland", "Tunisia", "United States", "Uruguay", "Wales", "Colombia", 
+  "South Korea", "Spain", "Switzerland", "Tunisia", "United States", "Uruguay", "Wales", "Colombia", "Palestine" 
+
 ];
