f62fdd9962c5ef2466563aa1e99c40a489d547f6
aleena-jaison <abi.alif@acetvm.com>
2026-07-08 21:26:46 +0530

    supabase updated

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
+import { clear } from 'console'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
